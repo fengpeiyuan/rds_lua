86e60599c0ede82efbf067a2a57f2452e98e0e92
fengpeiyuan <fengpeiyuan@gmail.com>
2016-08-17 09:44:43 +0800

    sample

--- del_keys.lua
+++ del_keys.lua
@@ -18,6 +18,10 @@ eval 'local cursor = "0" repeat local ret = redis.call("scan", cursor) cursor = 
 awk 'BEGIN{FS=" "} {print $6}' /var/log/redis_10567.log |xargs /opt/redis/bin/redis-cli -p 10567 del
 
 
+eval 'local cursor = "0" repeat local ret = redis.call("scan", cursor,"MATCH","ocard-*") cursor = ret[1] for _, key in ipairs(ret[2]) do redis.log(redis.LOG_NOTICE,key) end until cursor == "0" return' 0 
+
+awk 'BEGIN{FS=" "} {print $6}' /var/log/redis_10640.log |xargs /opt/redis/bin/redis-cli -p 10640 del
+
 -- 2.shell --
 
 #!/bin/sh
